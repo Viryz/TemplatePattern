5faa49443142ca489875b43e65ca428065ed1c34
Viryz <viryz1998@gmail.com>
2018-08-10 23:57:53 +0300

    first commit

---
+++ TemplatePattern/TemplatePattern/AbstractHouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    abstract class AbstractHouse
+    {
+        public void BuildFoundation()
+        {
+            Console.WriteLine("Build foundation");
+        }
+        public abstract void BuildWalls();
+        public abstract void BuildRoof();
+
+        public void Build()
+        {
+            BuildFoundation();
+            BuildWalls();
+            BuildRoof();
+        }
+    }
+}
